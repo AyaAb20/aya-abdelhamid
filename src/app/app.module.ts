283311956f6bc11d9cc62bd8f04e3d008981a2c4
Aya Ab <139051915+AyaAb20@users.noreply.github.com>
2023-08-26 10:34:33 +0100

    correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FeaturesComponent } from './features/features.component';
+import { ContactComponent } from './contact/contact.component';
+import { CertificatsComponent } from './certificats/certificats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FeaturesComponent,
+    ContactComponent,
+    CertificatsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/certificats/certificats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificats',
+  templateUrl: './certificats.component.html',
+  styleUrls: ['./certificats.component.css']
+})
+export class CertificatsComponent {
+
+}
